e9f05a145c9f98d6ec5e09ea78ee7a70d61c035b
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-27 00:12:54 -0500

    added initals

---
+++ script.js
@@ -0,0 +1,11 @@
+let random1 = Math.random();
+if (random1 < 0.5) {
+    console.log("Your answer is less than 0.5!!!");
+    console.log(random1);
+}
+
+let random2 = Math.random();
+if (random2 >= 0.5) {
+    console.log("Your greater is less than 0.5!!!");
+    console.log(random2);
+}
